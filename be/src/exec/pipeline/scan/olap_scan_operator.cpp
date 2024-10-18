66a0735059f906b567c31162f463080ff0e8cf01
trueeyu <lxhhust350@qq.com>
2024-10-18 14:17:38 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/pipeline/scan/olap_scan_operator.cpp
+++ be/src/exec/pipeline/scan/olap_scan_operator.cpp
@@ -61,7 +61,7 @@ OlapScanOperator::OlapScanOperator(OperatorFactory* factory, int32_t id, int32_t
 }
 
 OlapScanOperator::~OlapScanOperator() {
-    auto* scan_timer = _runtime_profile->get_counter("ScanTime");
+    auto* scan_timer = _unique_metrics->get_counter("ScanTime");
     if (scan_timer != nullptr) {
         VLOG(3) << "LXH: chunk_source active time: " << scan_timer->value();
         GlobalEnv::GetInstance()->_total_page_cache_io_time.fetch_add(scan_timer->value(), std::memory_order_relaxed);
