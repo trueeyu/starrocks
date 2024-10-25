4e5e993c1d6b9e905553dde16b40cedb7769674e
trueeyu <lxhhust350@qq.com>
2024-10-25 16:30:52 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/pipeline/scan/scan_operator.cpp
+++ be/src/exec/pipeline/scan/scan_operator.cpp
@@ -566,6 +566,7 @@ Status ScanOperator::_pickup_morsel(RuntimeState* state, int chunk_source_index)
         COUNTER_UPDATE(_morsels_counter, 1);
 
         {
+            VLOG(3) << "create chunk source: " << chunk_source_index;
             SCOPED_TIMER(_prepare_chunk_source_timer);
             _chunk_sources[chunk_source_index] = create_chunk_source(std::move(morsel), chunk_source_index);
             auto status = _chunk_sources[chunk_source_index]->prepare(state);
