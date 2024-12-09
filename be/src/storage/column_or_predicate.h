0cd85cda7ac187f7577de0c80ed8c6910b1acff6
trueeyu <lxhhust350@qq.com>
2024-12-09 11:33:53 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/storage/column_or_predicate.h
+++ be/src/storage/column_or_predicate.h
@@ -69,39 +69,4 @@ private:
     mutable std::vector<uint8_t> _buff;
 };
 
-class ColumnAndPredicate final : public ColumnPredicate {
-public:
-    explicit ColumnAndPredicate(const TypeInfoPtr& type_info, ColumnId cid) : ColumnPredicate(type_info, cid) {}
-
-    template <typename Container>
-    ColumnAndPredicate(const TypeInfoPtr& type_info, ColumnId cid, const Container& c)
-            : ColumnPredicate(type_info, cid), _child(c.begin(), c.end()) {}
-
-    void add_child(ColumnPredicate* child) { _child.emplace_back(child); }
-
-    template <typename Iterator>
-    void add_child(Iterator begin, Iterator end) {
-        _child.insert(_child.end(), begin, end);
-    }
-
-    Status evaluate(const Column* column, uint8_t* selection, uint16_t from, uint16_t to) const override;
-    Status evaluate_and(const Column* column, uint8_t* selection, uint16_t from, uint16_t to) const override;
-    Status evaluate_or(const Column* column, uint8_t* selection, uint16_t from, uint16_t to) const override;
-    bool filter(const BloomFilter& bf) const override { return true; }
-    bool zone_map_filter(const ZoneMapDetail& detail) const override;
-    bool can_vectorized() const override { return false; }
-    PredicateType type() const override { return PredicateType::kAnd; }
-    Datum value() const override { return {}; }
-    std::vector<Datum> values() const override { return std::vector<Datum>{}; }
-    Status convert_to(const ColumnPredicate** output, const TypeInfoPtr& target_type_info,
-                      ObjectPool* obj_pool) const override;
-    std::string debug_string() const override;
-
-private:
-    Status _evaluate(const Column* column, uint8_t* selection, uint16_t from, uint16_t to) const;
-
-    std::vector<const ColumnPredicate*> _child;
-    mutable std::vector<uint8_t> _buff;
-};
-
 } // namespace starrocks
