421eb9a6f5658f93d5c1ddea79741ab4524d34b0
trueeyu <lxhhust350@qq.com>
2025-07-22 15:46:01 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- java-extensions/java-utils/src/main/java/com/starrocks/utils/loader/ChildFirstClassLoader.java
+++ java-extensions/java-utils/src/main/java/com/starrocks/utils/loader/ChildFirstClassLoader.java
@@ -32,8 +32,6 @@ public class ChildFirstClassLoader extends URLClassLoader {
         ClassLoader.registerAsParallelCapable();
     }
 
-    private static final Logger LOG = LogManager.getLogger(ChildFirstClassLoader.class);
-
     private ParentClassLoader parentLoader;
     private ArrayList<String> parentFirstClass;
 
