a041ed411e72888080eb1e419e4122e453db4428
trueeyu <lxhhust350@qq.com>
2025-09-18 03:37:44 +0800

    Revert "test"
    This reverts commit d8bc291c2a0d7190692004f2530a174a5b196300.

--- be/src/column/fixed_length_column_base.h
+++ be/src/column/fixed_length_column_base.h
@@ -235,7 +235,15 @@ public:
 
     std::string get_name() const override;
 
-    Container& get_data() { return _data; }
+    Container& get_data() {
+        // Note: not thread safe !
+        if (!_resource.empty()) {
+            auto span = _resource.span<T>();
+            _data.assign(span.begin(), span.end());
+            _resource.reset();
+        }
+        return _data;
+    }
 
     const ImmContainer immutable_data() const {
         if (!_resource.empty()) {
