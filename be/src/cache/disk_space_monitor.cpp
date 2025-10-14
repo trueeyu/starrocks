c9bc4f3f0fb46f93a249b4b676d78abb09ab5b85
trueeyu <lxhhust350@qq.com>
2025-10-14 14:17:25 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/cache/disk_space_monitor.cpp
+++ be/src/cache/disk_space_monitor.cpp
@@ -96,7 +96,7 @@ Status DiskSpace::_update_disk_stats() {
     auto& space_info = ret.value();
     _disk_stats.capacity_bytes = space_info.capacity;
     _disk_stats.available_bytes = space_info.available;
-    VLOG(2) << "Get disk statistics, capacity: " << _disk_stats.capacity_bytes
+    LOG(ERROR) << "LXH: " << "Get disk statistics, capacity: " << _disk_stats.capacity_bytes
             << ", available: " << _disk_stats.available_bytes << ", used_bytes: " << _disk_stats.used_bytes();
 
     return Status::OK();
