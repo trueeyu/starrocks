e94c4c4226cba92f4257db1dd20a82f7ca3ff412
trueeyu <lxhhust350@qq.com>
2025-10-15 14:22:22 +0800

    Fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/formats/parquet/group_reader.cpp
+++ be/src/formats/parquet/group_reader.cpp
@@ -298,7 +298,9 @@ StatusOr<size_t> GroupReader::_read_range_round_by_round(const Range<uint64_t>& 
         SlotId slot_id = column.slot_id();
         auto st = _column_readers[slot_id]->read_range(range, filter, (*chunk)->get_column_by_slot_id(slot_id));
         if (!st.ok()) {
+            auto tmp_column = (*chunk)->get_column_by_slot_id(slot_id);
             LOG(ERROR) << "LXH: C_ERROR: " << _column_readers[slot_id]->get_column_parquet_field()->debug_string();
+            LOG(ERROR) << "LXH: C_COLUMN: " << slot_id << ":" << tmp_column->get_name();
             return st;
         }
         //RETURN_IF_ERROR(_column_readers[slot_id]->read_range(range, filter, (*chunk)->get_column_by_slot_id(slot_id)));
