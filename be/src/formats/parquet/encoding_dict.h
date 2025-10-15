8ba09bbb56a24091aea452f0df61fd2203e0fdfd
trueeyu <lxhhust350@qq.com>
2025-10-15 11:16:57 +0800

    Fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/formats/parquet/encoding_dict.h
+++ be/src/formats/parquet/encoding_dict.h
@@ -634,7 +634,7 @@ private:
             }
             ret = dst->append_strings_overflow(_slices.data(), _slices.size(), _max_value_length);
             if (UNLIKELY(!ret)) {
-                return Status::InternalError("DictDecoder append strings to column failed 3");
+                return Status::InternalError(fmt::format("DictDecoder append strings to column failed 3: {}", ret));
             }
         }
         return Status::OK();
