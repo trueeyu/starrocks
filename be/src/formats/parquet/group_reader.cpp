3213cd5217844f069f0449907495098eaebdbbad
trueeyu <lxhhust350@qq.com>
2025-10-22 18:06:52 +0800

    Fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/formats/parquet/group_reader.cpp
+++ be/src/formats/parquet/group_reader.cpp
@@ -283,28 +283,6 @@ StatusOr<size_t> GroupReader::_read_range_round_by_round(const Range<uint64_t>& 
     DeferOp defer([&]() { _column_read_order_ctx->update_ctx(round_cost, first_selectivity); });
     size_t hit_count = 0;
 
-    LOG(ERROR) << "LXH_RESER: " << (_param.reserved_field_slots != nullptr);
-
-    if (_param.reserved_field_slots != nullptr) {
-        LOG(ERROR) << "LXH_RESER2: " << _param.reserved_field_slots->size();
-        for (const auto* slot : *_param.reserved_field_slots) {
-            SlotId slot_id = slot->id();
-            RETURN_IF_ERROR(
-                    _column_readers[slot_id]->read_range(range, filter, (*chunk)->get_column_by_slot_id(slot_id)));
-            if (_left_no_dict_filter_conjuncts_by_slot.find(slot_id) != _left_no_dict_filter_conjuncts_by_slot.end()) {
-                SCOPED_RAW_TIMER(&_param.stats->expr_filter_ns);
-                std::vector<ExprContext*> ctxs = _left_no_dict_filter_conjuncts_by_slot.at(slot_id);
-                auto temp_chunk = std::make_shared<Chunk>();
-                temp_chunk->columns().reserve(1);
-                ColumnPtr& column = (*chunk)->get_column_by_slot_id(slot_id);
-                temp_chunk->append_column(column, slot_id);
-                ASSIGN_OR_RETURN(hit_count, ExecNode::eval_conjuncts_into_filter(ctxs, temp_chunk.get(), filter));
-                if (hit_count == 0) {
-                    break;
-                }
-            }
-        }
-    }
     for (int col_idx : read_order) {
         auto& column = _param.read_cols[col_idx];
         round_cost += _column_read_order_ctx->get_column_cost(col_idx);
