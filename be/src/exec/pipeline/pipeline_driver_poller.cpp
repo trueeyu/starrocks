8c3b3ba96e7bc56c19646d4a994597a41d80601e
trueeyu <lxhhust350@qq.com>
2025-12-12 17:05:50 +0800

    test 1
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/pipeline/pipeline_driver_poller.cpp
+++ be/src/exec/pipeline/pipeline_driver_poller.cpp
@@ -110,6 +110,7 @@ void PipelineDriverPoller::run_internal() {
                     remove_blocked_driver(_local_blocked_drivers, driver_it);
                     ready_drivers.emplace_back(driver);
                 } else if (driver->pending_finish()) {
+                    LOG(ERROR) << "LXH: pooller pending finishing";
                     if (driver->is_still_pending_finish()) {
                         ++driver_it;
                     } else {
