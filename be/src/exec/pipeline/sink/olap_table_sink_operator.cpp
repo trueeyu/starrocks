6bf1403e7e2091609be433301a01c842c1631942
trueeyu <lxhhust350@qq.com>
2025-12-17 11:57:46 +0800

    test 1
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/pipeline/sink/olap_table_sink_operator.cpp
+++ be/src/exec/pipeline/sink/olap_table_sink_operator.cpp
@@ -148,10 +148,10 @@ Status OlapTableSinkOperator::push_chunk(RuntimeState* state, const ChunkPtr& ch
         LOG(ERROR) << "LXH: LocalTabletsChannel::push_chunk 1: " << (_automatic_partition_chunk != nullptr);
         return Status::InternalError("OlapTableSinkOperator::push_chunk");
     } else if (config::lxh_mode == 2) {
-        sleep(5);
+        sleep(2);
         LOG(ERROR) << "LXH: LocalTabletsChannel::push_chunk 2: " << (_automatic_partition_chunk != nullptr);
     } else if (config::lxh_mode == 3) {
-        sleep(5);
+        sleep(3);
         LOG(ERROR) << "LXH: LocalTabletsChannel::push_chunk 3: " << (_automatic_partition_chunk != nullptr);
     }
     if (_automatic_partition_chunk) {
