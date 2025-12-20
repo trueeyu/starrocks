687eb7988941dd6b66004cc8ad7721a56c1ba108
trueeyu <lxhhust350@qq.com>
2025-12-20 13:13:05 +0800

    test 1
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/test/util/block_compression_test.cpp
+++ be/test/util/block_compression_test.cpp
@@ -70,9 +70,9 @@ public:
 
 static std::string generate_str(size_t len) {
     static char charset[] =
-            "0123456789"
-            "0123456789"
-            "0123445789";
+            "01234"
+            "01234"
+            "01234";
     std::string result;
     result.resize(len);
     for (int i = 0; i < len; ++i) {
