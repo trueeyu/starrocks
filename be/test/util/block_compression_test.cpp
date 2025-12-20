d3ca51be954865e053f982a7e2d17efd21f6a98b
trueeyu <lxhhust350@qq.com>
2025-12-20 22:36:28 +0800

    test 1
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/test/util/block_compression_test.cpp
+++ be/test/util/block_compression_test.cpp
@@ -195,7 +195,6 @@ TEST_F(BlockCompressionTest, lxh_test2) {
 
     LZ4F_resetDecompressionContext(dctx);
 
-    /*
     {
         size_t const c0SizeBound = LZ4F_compressFrameBound(0, NULL);
         LOG(ERROR) << "REAL_SIZE: " << c0SizeBound << std::endl;
@@ -218,7 +217,6 @@ TEST_F(BlockCompressionTest, lxh_test2) {
     }
 
     LZ4F_resetDecompressionContext(dctx);
-    */
 
     {
         const char s9Buffer[9] = {0};
