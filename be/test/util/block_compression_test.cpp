e54f64d67ce927be8c00d4d7ba360d7d2ae5c993
trueeyu <lxhhust350@qq.com>
2025-12-20 22:30:38 +0800

    test 1
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/test/util/block_compression_test.cpp
+++ be/test/util/block_compression_test.cpp
@@ -264,7 +264,7 @@ TEST_F(BlockCompressionTest, lxh_test2) {
             /* decompress it, but do not complete the process - state not terminated correctly */
             {
                 size_t dstSize = sizeof(d9Buffer);
-                size_t srcSize = 32;
+                size_t srcSize = 20;
                 size_t const d9Size = LZ4F_decompress(dctx, d9Buffer, &dstSize, c9Buffer, &srcSize, NULL);
                 if (LZ4F_isError(d9Size)) {
                     std::cout << "decompress_4: " << d9Size << ":" << LZ4F_getErrorName(d9Size) << ":" << dstSize << std::endl;
