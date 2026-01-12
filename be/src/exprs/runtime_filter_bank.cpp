9833dc54909c346c97cd604d0719738cd6c55223
trueeyu <lxhhust350@qq.com>
2026-01-12 10:41:54 +0800

    fix conflict
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exprs/runtime_filter_bank.cpp
+++ be/src/exprs/runtime_filter_bank.cpp
@@ -849,6 +849,7 @@ void RuntimeFilterProbeCollector::evaluate(Chunk* chunk, RuntimeMembershipFilter
         size_t after = chunk->num_rows();
         eval_context.join_runtime_filter_output_counter->update(after);
         eval_context.join_runtime_filter_eval_counter->update(eval_context.run_filter_nums);
+        LOG(ERROR) << "LXH: before_after: " << before << ":" << after;
     }
 }
 
