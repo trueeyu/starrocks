211b2cb27893af946ae0136168c32e6f4ec86184
trueeyu <lxhhust350@qq.com>
2026-02-03 15:20:43 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/geo/geo_types.cpp
+++ be/src/geo/geo_types.cpp
@@ -136,6 +136,8 @@ static GeoParseStatus to_s2loop(const GeoCoordinateList& coords, std::unique_ptr
     if (!(*loop)->IsValid()) {
         return GEO_PARSE_LOOP_INVALID;
     }
+    // if (bound_.lng().GetLength() < M_PI) return true;
+    LOG(ERROR) << "LXH: BOUND: " << (*loop)->GetRectBound().lng().GetLength();
     LOG(ERROR) << "LXH: NORMAL: " << (*loop)->IsNormalized() << ":" << (*loop)->is_hole();
     (*loop)->Normalize();
     return GEO_PARSE_OK;
