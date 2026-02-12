b1acd369b54fe2f202cd5c7d11c0e38eaa1d716e
trueeyu <lxhhust350@qq.com>
2026-02-12 16:57:02 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exprs/string_functions.h
+++ be/src/exprs/string_functions.h
@@ -806,8 +806,9 @@ StatusOr<ColumnPtr> StringFunctions::field(FunctionContext* context, const Colum
         return result.build(true);
     } else if (state != nullptr) {
         if (state->all_const) {
-            LOG(ERROR) << "LXH: field all const";
             const auto list_col = context->get_constant_column(0);
+            const ColumnPtr& c = ColumnHelper::as_raw_column<ConstColumn>(list_col)->data_column();
+            LOG(ERROR) << "LXH: field all const: " << c->get_name();
             const auto list_val = ColumnHelper::get_const_value<Type>(list_col);
             auto it = state->mp.find(list_val);
             if (it != state->mp.end()) {
