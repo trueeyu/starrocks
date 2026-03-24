43003f8c691134f949764ff00e3bd98400b4c59c
trueeyu <lxhhust350@qq.com>
2026-03-24 09:24:13 +0800

    fix compile
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/bench/binary_column_copy_bench.cpp
+++ be/src/bench/binary_column_copy_bench.cpp
@@ -124,17 +124,17 @@ static void bench_func(benchmark::State& state) {
 }
 
 static void process_args(benchmark::internal::Benchmark* b) {
-    b->Args({1, 4096})->Iterations(1000);
-    b->Args({2, 4096})->Iterations(1000);
+    b->Args({1, 4096})->Iterations(100000);
+    b->Args({2, 4096})->Iterations(100000);
 
-    b->Args({1, 40960})->Iterations(100);
-    b->Args({2, 40960})->Iterations(100);
+    b->Args({1, 40960})->Iterations(10000);
+    b->Args({2, 40960})->Iterations(10000);
 
-    b->Args({1, 409600})->Iterations(10);
-    b->Args({2, 409600})->Iterations(10);
+    b->Args({1, 409600})->Iterations(100);
+    b->Args({2, 409600})->Iterations(100);
 
-    b->Args({1, 4096000})->Iterations(10);
-    b->Args({2, 4096000})->Iterations(10);
+    b->Args({1, 4096000})->Iterations(100);
+    b->Args({2, 4096000})->Iterations(100);
 }
 
 BENCHMARK(bench_func)->Apply(process_args);
