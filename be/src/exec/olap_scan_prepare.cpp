8691d1bbde0ddf94b952318902e55a6d9dfe24ba
trueeyu <lxhhust350@qq.com>
2026-04-05 19:11:06 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/olap_scan_prepare.cpp
+++ be/src/exec/olap_scan_prepare.cpp
@@ -18,6 +18,8 @@
 #include <variant>
 
 #include "base/orlp/pdqsort.h"
+#include "column/column_helper.h"
+#include "column/raw_data_visitor.h"
 #include "column/runtime_type_traits.h"
 #include "common/config_scan_io_fwd.h"
 #include "exprs/binary_predicate.h"
@@ -185,11 +187,15 @@ static bool get_predicate_value(ObjectPool* obj_pool, const SlotDescriptor& slot
         // |column_ptr| will be released after this method return, have to ensure that
         // the corresponding external storage will not be deallocated while the slice
         // still been used.
-        const auto* slice = reinterpret_cast<const Slice*>(data->raw_data());
-        std::string* str = obj_pool->add(new std::string(slice->data, slice->size));
+        const auto slice = GetContainer<TYPE_VARCHAR>::get_data(data.get())[0];
+        std::string* str = obj_pool->add(new std::string(slice.data, slice.size));
         *value = *str;
     } else {
-        *value = *reinterpret_cast<const ValueType*>(data->raw_data());
+        RawDataVisitor visitor;
+        if (!data->accept(&visitor).ok()) {
+            return false;
+        }
+        *value = *reinterpret_cast<const ValueType*>(visitor.result());
         if (r->type().is_decimalv3_type()) {
             return check_decimal_overflow<ValueType>(r->type().precision, *value);
         }
