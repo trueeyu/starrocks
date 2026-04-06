2ab8a6dae6fdbf9751493f76c69c77013fcc29c2
trueeyu <lxhhust350@qq.com>
2026-04-06 18:19:55 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/storage/rowset/binary_dict_page.cpp
+++ be/src/storage/rowset/binary_dict_page.cpp
@@ -248,8 +248,7 @@ Status BinaryDictPageDecoder<Type>::next_batch(const SparseRange<>& range, Colum
 
     RETURN_IF_ERROR(_data_page_decoder->next_batch(range, _vec_code_buf.get()));
     size_t nread = _vec_code_buf->size();
-    using cast_type = StorageCppType<TYPE_INT>;
-    const auto* codewords = reinterpret_cast<const cast_type*>(_vec_code_buf->raw_data());
+    const auto* codewords = GetContainer<TYPE_INT>::get_data(_vec_code_buf.get()).data();
 
     static_assert(sizeof(Slice) == sizeof(int128_t));
     auto slices_data = std::make_unique_for_overwrite<uint8_t[]>(nread * sizeof(Slice));
@@ -354,8 +353,7 @@ Status BinaryDictPageDecoder<Type>::next_batch_with_filter(
         return Status::OK();
     }
 
-    using cast_type = StorageCppType<TYPE_INT>;
-    const auto* codewords = reinterpret_cast<const cast_type*>(_vec_code_buf->raw_data());
+    const auto* codewords = GetContainer<TYPE_INT>::get_data(_vec_code_buf.get()).data();
 
     // Step 3: Update selection based on dictionary selection and collect matching slices
     std::vector<Slice> selected_slices;
@@ -407,8 +405,7 @@ Status BinaryDictPageDecoder<Type>::read_by_rowids(const ordinal_t first_ordinal
         *count = 0;
         return Status::OK();
     }
-    using cast_type = StorageCppType<TYPE_INT>;
-    const auto* codewords = reinterpret_cast<const cast_type*>(_vec_code_buf->raw_data());
+    const auto* codewords = GetContainer<TYPE_INT>::get_data(_vec_code_buf.get()).data();
     auto slices_data = std::make_unique_for_overwrite<uint8_t[]>(read_count * sizeof(Slice));
     Slice* slices = reinterpret_cast<Slice*>(slices_data.get());
     if constexpr (Type == TYPE_CHAR) {
